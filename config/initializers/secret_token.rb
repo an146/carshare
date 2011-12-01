c68f3dd03a8481f3e611059306c994690aef3354
Anton Novikov <an146@ya.ru>
2011-12-02 01:36:39 +0400

    initial commit, just `rails new`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carshare::Application.config.secret_token = 'd3f8f4ab99152474a8043f342d1259be14af333bfebfb3172b75bfe46bdc33940e96ca190399fdd34e24e7eca638fae10a2c6d713930d44604e12b99b70a0192'
